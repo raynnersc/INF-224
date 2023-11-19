00c6cda90d16b1ad2408fc18ed59e52fd58143c7
raynnersc <raynnerschnneider@hotmail.com>
2023-11-19 20:41:23 +0100

    (TD INF224): Java Swing - question 2

--- swing/ClientWindow.java
+++ swing/ClientWindow.java
@@ -1,55 +1,77 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class ClientWindow extends JFrame implements ActionListener {
+public class ClientWindow extends JFrame {
     private static final long serialVersionUID = 1L;
     JTextArea textArea;
     JButton button1, button2, button3;
+    JMenuBar menuBar;
+    JMenu menu;
+    JToolBar toolBar;
 
     public ClientWindow() {
         super("Client Window");
         setLayout(new BorderLayout());
         textArea = new JTextArea(20, 40);
 
-        JPanel buttonPanel = new JPanel();
-        
+        menuBar = new JMenuBar();
+        menu = new JMenu("Menu");
+        setJMenuBar(menuBar);
+        menuBar.add(menu);
+
+        // button1 = new JButton("Button 1");
+        // button2 = new JButton("Button 2");
+        // button3 = new JButton("Exit");
+
+        Action actionButton1 = new ButtonAction("Button 1");
+        Action actionButton2 = new ButtonAction("Button 2");
+        Action actionButton3 = new ExitAction();
+
+        menu.add(new JMenuItem(actionButton1));
+        menu.add(new JMenuItem(actionButton2));
+        menu.addSeparator();
+        menu.add(new JMenuItem(actionButton3));
+
+        toolBar = new JToolBar();
+        toolBar.add(new JButton(actionButton1));
+        toolBar.add(new JButton(actionButton2));
+        toolBar.add(new JButton(actionButton3));
+        add(toolBar, BorderLayout.NORTH);
+
         JScrollPane scrollPane = new JScrollPane(textArea);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         add(scrollPane, BorderLayout.CENTER);
 
-        button1 = new JButton("Button 1");
-        button1.addActionListener(this);
-        buttonPanel.add(button1);
-        
-        button2 = new JButton("Button 2");
-        button2.addActionListener(this);
-        buttonPanel.add(button2);
-
-        button3 = new JButton("Button 3");
-        button3.addActionListener(this);
-        buttonPanel.add(button3);
-
-        add(buttonPanel, BorderLayout.NORTH);
-        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new ClientWindow();
+    private class ButtonAction extends AbstractAction {
+        public ButtonAction(String name) {
+            super(name);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            textArea.append(getValue(NAME) + " was pressed\n");
+        }
     }
 
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button1) {
-            textArea.append("Button 1 was pressed\n");
-        } else if (e.getSource() == button2) {
-            textArea.append("Button 2 was pressed\n");
-        } else if (e.getSource() == button3) {
-            textArea.append("Button 3 was pressed\n");
+    private class ExitAction extends AbstractAction {
+        public ExitAction() {
+            super("Exit");
         }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ClientWindow();
     }
 }
